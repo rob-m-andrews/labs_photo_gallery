d780706a26c511a0181456f3bf0497e467084d9f
Rob Andrews <andrews.robert.m@gmail.com>
2016-02-05 17:25:16 -0500

    Adds silly watermark and tweaks load times.

--- public/photoGallery.js
+++ public/photoGallery.js
@@ -1,25 +1,28 @@
 (function() {
   var FADE_EFFECT_DELAY = 500;
-  var PHOTO_RELOAD_TIME = 5000;
+  var PHOTO_RELOAD_TIME = 4000;
+  var TRANSITION_SELECTOR = '.transition';
+  var TRANSITION_SHOWN_CLASS = 'transition--shown';
 
   function loadNewGalleryImage() {
     var image = new Image();
-    var $transition = $('.transition');
+    var $transition = $(TRANSITION_SELECTOR);
 
     image.onload = function() {
-      $transition.addClass('transition--shown');
+      $transition.addClass(TRANSITION_SHOWN_CLASS);
 
       setTimeout(function() {
         $('.photo').css({
           'background-image': 'url(' + image.src + ')'
         });
-        $transition.removeClass('transition--shown');
+        $transition.removeClass(TRANSITION_SHOWN_CLASS);
       }, FADE_EFFECT_DELAY);
     };
     image.src = '/photo' + Date.parse(new Date());
   };
 
   $(function() {
+    loadNewGalleryImage();
     setInterval(loadNewGalleryImage, PHOTO_RELOAD_TIME);
   });
 }());
